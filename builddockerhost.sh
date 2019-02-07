c7a6b1e109fc20193127c549b3b1681c41363ab4
diarmuidw <diarmuid@bluekulu.com>
2019-02-07 21:37:42 +0000

    Create builddockerhost.sh

---
+++ builddockerhost.sh
@@ -0,0 +1,50 @@
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+	
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+
+ mkdir Readytalk
+ 
+ cd Readytalk/
+ 
+ git clone https://github.com/diarmuidw/rtpengine-docker.git
+ 
+ cd rtpengine-docker/
+ 
+ docker build -t dwrtpengine .
+
+ 
+ chmod +x docker_run.sh
+
+./docker_run.sh
+
+
+ netstat -tulpn
+ 
+ 
+ apt-get install ngrep
+
+ ngrep -p -q -W byline port 22222
+
+ 
+ 
